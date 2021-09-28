87cc7bcb3f5fb5f18888a27a5e1cd2f6308fffb6
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-09-28 14:09:16 +0530

    new program

---
+++ countProgram.py
@@ -0,0 +1,18 @@
+def np(lst):
+    even = 0
+    odd = 0
+
+    for i in lst:
+        if i%2==0:
+            even = even + 1
+        else :
+            odd = odd + 1
+
+    return even,odd
+
+lst = [25,38,46,25,65,98,73,65,45,41]
+
+even,odd = np(
+    lst)
+
+print('even : {} and odd : {}'.format(even,odd))
--- new calulator.py
+++
@@ -1,9 +0,0 @@
-def myinfo(name, **data):
-    print(name)
-
-    for i,j in data.items():
-        print(i,j)
-
-
-
-myinfo('Tanmay',age=16,city=': pune',mob=9309318,language=': marathi',)
