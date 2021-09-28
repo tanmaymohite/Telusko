1215582210f350f99d9bb061688c30111bc81bfa
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-09-28 14:18:10 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,23 @@
+n = input('How much do you want : ')
+n  = int(n)
+
+
 def fib(n):
      a = 0
      b = 1
 
-     print(a)
-     print(b)
+     if n==1:
+         print(a)
+     else:
 
-     for i in range(2,n):
-         c = a + b
-         a = b
-         b = c
-         print(c)
+         print(a)
+         print(b)
 
-fib(5)
+         for i in range(2,n):
+             c = a + b
+             a = b
+             b = c
+             print(c)
+
+fib(n)
 
