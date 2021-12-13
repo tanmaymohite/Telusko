524f35578244314edb76b1612cbb9538df8d0144
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-12-13 16:35:20 +0530

    Update determinants 3X3.py

--- determinants 3X3.py
+++ determinants 3X3.py
@@ -27,14 +27,60 @@ h = int(h)
 i = input('Enter the value of i : ')
 i = int(i)
 
+v1 = input('Value of equation 1: ')
+v1 = int(v1)
+
+v2 = input('Value of equation 2: ')
+v2 = int(v2)
+
+v3 = input('Value of equation 3: ')
+v3 = int(v3)
+
 D1 = a*((e*i)-(f*h))
 D2 = -b*((d*i)-(f*g))
 D3 = c*((d*h)-(e*g))
 
 
 D = D1+D2+D3
+print('D: '+str(D))
 
-print('Ans: '+str(D))
+# now code for Dx
 
+Dx1 = v1*((e*i)-(f*h))
+Dx2 = -b*((v2*i)-(f*v3))
+Dx3 = c*((v2*h)-(v3*e))
 
+Dx = Dx1+Dx2+Dx3
 
+print('Dx: '+str(Dx))
+
+# now code for Dy
+
+Dy1 = a*((v2*i)-(f*v3))
+Dy2 = -v1*((d*i)-(g*f))
+Dy3 = c*((d*v3)-(g*v2))
+
+Dy = Dy1+Dy2+Dy3
+print('Dy: '+str(Dy))
+
+# now code for Dz
+
+Dz1 = a*((e*v3)-(h*v2))
+Dz2 = -b*((d*v3)-(g*v2))
+Dz3 = v1*((d*h)-(g*e))
+
+Dz = Dz1+Dz2+Dz3
+
+print('Dz: '+str(Dz))
+
+x = Dx/D
+print('x:'+str(x))
+
+y = Dy/D
+print('y:'+str(y))
+
+z = Dz/D
+print('z:'+str(z))
+
+set = [x,y,z]
+print(set)
