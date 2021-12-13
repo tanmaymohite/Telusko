462dc050a3b3b20f02076356f374454145a8a012
tanmaymohite <87615488+tanmaymohite@users.noreply.github.com>
2021-12-13 11:44:36 +0530

    3*3

---
+++ determinants 3X3.py
@@ -0,0 +1,33 @@
+print('************ 3X3 determinants *************')
+
+a = input('Enter the value of a: ')
+a = int(a)
+
+b = input('Enter the value of b: ')
+b = int(b)
+
+c = input('Enter the value of c: ')
+c = int(c)
+
+d = input('Enter thevalue of d: ')
+d = int(d)
+
+e = input('Enter the value of e: ')
+e = int(e)
+
+f = input('Enter the value f: ')
+f = int(f)
+
+g = input('Enter the value of g: ')
+g = int(g)
+
+h = input('Enter the value of h: ')
+h = int(h)
+
+i = input('Enter the value of i : ')
+i = int(i)
+
+D = a*(e*i)-(f*h)
+print(D)
+
+
